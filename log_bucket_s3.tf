0f1bd771bb5f617370da037835d119f65c39619e
Yuta Ushijima <register@yuta-u.com>
2019-09-11 09:22:33 +0900

    fix: Defiend bucket policy for log_bucket_s3

--- log_bucket_s3.tf
+++ log_bucket_s3.tf
@@ -9,3 +9,28 @@ resource "aws_s3_bucket" "alb_log" {
       }
   }
 }
+
+resource "aws_s3_bucket_policy" "alb_log" {
+    bucket = aws_s3_bucket.alb_log.id
+    policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+      effect = "Allow"
+      actions = ["s3:PutObject"]
+      resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+      principals {
+          type = "AWS"
+          identifiers = ["582318560864"]
+      }
+  }
+}
+
+# S3バケットの強制削除
+# resource "aws_s3_bucket" "force_destroy" {
+#   bucket = "force-destroy-pragmatic-terraform-on-aws"
+#   force_destroy = true
+# }
+
